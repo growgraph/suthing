ccca684d4090483f1e2ecb59adf9b03ff87502c8
Alexander Belikov <abelikov@gmail.com>
2023-04-03 18:18:37 +0200

    added SProfiler

--- suthing/__init__.py
+++ suthing/__init__.py
@@ -1,4 +1,4 @@
 from .compare import equals
-from .decorate import Report, Return, secureit, timeit
+from .decorate import Report, Return, secureit, timeit, SProfiler, profile
 from .file_handle import FileHandle
 from .timer import Timer
