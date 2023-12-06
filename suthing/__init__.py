3e4fd9255143c61a9e4eb92fcd8f43f46a1c0890
Alexander Belikov <abelikov@gmail.com>
2023-12-06 18:03:49 +0100

    added configs

--- suthing/__init__.py
+++ suthing/__init__.py
@@ -1,4 +1,11 @@
 from .compare import equals
+from .config import (
+    ArangoConnectionConfig,
+    ConnectionKind,
+    DBConnectionConfig,
+    Neo4jConnectionConfig,
+    WSGIConfig,
+)
 from .decorate import Report, Return, SProfiler, profile, secureit, timeit
 from .file_handle import FileHandle
 from .timer import Timer
